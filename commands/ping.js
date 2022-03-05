58a5932ff05bee2668b01d8b5aa61f5c5e609450
GGORG <ggorg@onet.pl>
2022-03-05 12:36:25 +0100

    improved /ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,15 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { stripIndent } = require('common-tags');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Test if the bot is working'),
 	async execute(interaction) {
-		await interaction.reply(`Pong! Latency is ${interaction.client.ws.ping}ms.`);
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		interaction.editReply(stripIndent`
+		Pong!
+		Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms
+		WebSocket latency: ${interaction.client.ws.ping}ms`);
 	},
 };
