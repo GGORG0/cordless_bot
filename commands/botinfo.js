6ac7aedfd8add786cf499467bbfcb542b7b5b232
GGORG <ggorg@onet.pl>
2022-03-17 20:49:59 +0100

    added cpu and ram usage to botinfo

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const ms = require('ms');
+const os = require('node:os');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -17,6 +18,9 @@ module.exports = {
 				{ name: 'Uptime', value: `${ms(interaction.client.uptime)}`, inline: true },
 				{ name: 'Author', value: `<@${interaction.client.application?.owner?.id}>`, inline: true },
 				{ name: 'User ID', value: `${interaction.client.user.id}`, inline: true },
+				{ name: 'Node.js version', value: `${process.version}`, inline: true },
+				{ name: 'RAM Usage', value: `${os.freemem()} free out of ${os.totalmem()}`, inline: true },
+				{ name: 'CPU Usage', value: `${os.cpus()}`, inline: true },
 				{ name: 'Source code', value: '[Click me!](https://gh.ggorg.tk/cordless_bot)', inline: true },
 				{ name: 'Support server', value: '[Click me!](https://discord.gg/dRNxjZphvu)', inline: true },
 				{ name: 'Add me', value: '[Click me!](https://discord.com/api/oauth2/authorize?client_id=949390883038572605&permissions=540404806&scope=applications.commands%20bot)', inline: true },
